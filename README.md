9727d46159bececd802dca416c4ad3c5d42e3b59
Shana <devfusediboot@protonmail.com>
2020-10-11 19:00:33 -0700

    added manjaro support

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # dotfiles
-My dotfiles repo and bootstrapping script for Arch (and Arch-based) and Fedora currently.
+My dotfiles repo and bootstrapping script for Arch (and Manjaro) and Fedora currently.
 This does not install desktop environments or anything, you'll still have to do that manually!
 However, it does install most of the applications I use on my computer, if on Arch or Fedora.
 
--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -3,7 +3,10 @@ echo "Installing packages..."
 DIST=$(awk '/^ID=/' /etc/*-release | awk -F'=' '{ print tolower($2) }')
 
 if [[ $DIST == "arch" ]]; then
-    echo 'Arch'
+    echo 'Installing packages for Arch'
+    sudo pacman -Syu $(cat packages/pacman)
+elif [[ $DIST == "manjaro" ]]; then
+    echo 'Installing packages for Manjaro'
     sudo pacman -Syu $(cat packages/pacman)
 elif [[ $DIST == "fedora" ]]; then
     echo "Executing Fedora script..."
