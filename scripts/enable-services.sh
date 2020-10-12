37174c1c16702675b95b8444656beb8b0009361a
Shana <devfusediboot@protonmail.com>
2020-10-11 20:23:05 -0700

    added detection in enable-services script

--- scripts/enable-services.sh
+++ scripts/enable-services.sh
@@ -1,1 +1,7 @@
-sudo systemctl enable --now libvirtd
+#!/bin/bash
+if [[ $(uname -s) == "Linux" ]]; then # assumes systemd
+    echo "Enabling services..."
+    sudo systemctl enable --now libvirtd
+elif [[ $(uname -s) == "Darwin" ]]; then
+    echo "Nothing to enable atm"
+fi
