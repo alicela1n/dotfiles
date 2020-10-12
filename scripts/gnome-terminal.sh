50b71dda7485d07e28ee90d53a2fa5e299bd6a8e
Shana <devfusediboot@protonmail.com>
2020-10-12 16:18:35 -0700

    added intellij and vscode to macOS packages

--- scripts/gnome-terminal.sh
+++ scripts/gnome-terminal.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 echo "Do you use GNOME terminal and would you like to apply my config?"
-while true
-do
-read -r -p "[Y/n] " input
-case $input in
-    [yY][eE][sS]|[yY])
-    echo "Backing up current config..."
-    pushd gnome-terminal >/dev/null
-    ./backup.sh
-    echo "Backed up current config to gnome-terminal/gnome-terminal-settings-backup.dconf"
-    echo "Now applying new settings..."
-    ./settings.sh
-    popd >/dev/null
-    break
-    ;;
+   while true
+    do
+    read -r -p "[Y/n] " input
+        case $input in
+        [yY][eE][sS]|[yY])
+        echo "Backing up current config..."
+        pushd gnome-terminal >/dev/null
+        ./backup.sh
+        echo "Backed up current config to gnome-terminal/gnome-terminal-settings-backup.dconf"
+        echo "Now applying new settings..."
+        ./settings.sh
+        popd >/dev/null
+        break
+        ;;
     [nN][oO]|[nN])
     echo "Skipping"
     break
@@ -23,4 +23,3 @@ case $input in
     ;;
 esac
 done
-
--- scripts/install-packages-macos.sh
+++ scripts/install-packages-macos.sh
@@ -9,7 +9,7 @@ if [ ! -f /usr/local/bin/brew ]; then
     # Install homebrew
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" 
 else
-    echo "Homebrew already installed, skipping..."
+    echo "Homebrew already installed, skipping..."
 fi
 
 brew install $(cat packages/homebrew)
