acbfadc8dbb824cbb42fc71ddd8c4087505e2042
Shana <devfusediboot@protonmail.com>
2020-10-11 18:36:04 -0700

    of course gvim messed up my install packages script

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 echo "Installing packages..."
+DIST=$(awk '/^ID=/' /etc/*-release | awk -F'=' '{ print tolower($2) }')
 
-if [[ 'which pacman' ]]; then
+if [[ $DIST == "arch" ]]; then
+    echo 'Arch'
     sudo pacman -S $(cat packages/pacman)
-elif [[ 'which dnf' ]]; then
-    sudo dnf install $(cat packages/dnf)
+elif [[ $DIST == "fedora" ]]; then
+    echo "Executing Fedora script..."
+    sudo scripts/install-packages-fedora.sh
 else
     echo "Unknown distro, install packages yourself"
 fi
