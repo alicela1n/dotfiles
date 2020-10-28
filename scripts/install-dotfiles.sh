86e3426ca2f0a4a9e8a5606a7641a19ce60b7dcd
Lyra <devfusediboot@protonmail.com>
2020-10-27 20:29:34 -0700

    made changes and added neofetch config

--- scripts/install-dotfiles.sh
+++ scripts/install-dotfiles.sh
@@ -3,8 +3,9 @@ git submodule update --init --depth 1
 cp -v dotfiles/tmux.conf ~/.tmux.conf
 cp -v dotfiles/vimrc ~/.vimrc
 cp -v dotfiles/Xresources ~/.Xresources
+mkdir ~/.zsh
 cp -vr dotfiles/zsh ~/.zsh
 cp -r dotfiles/zshenv ~/.zshenv
 cp -r dotfiles/zshrc ~/.zshrc
-mkdir -p ~/.config/nvim
-cp -vr dotfiles/config/nvim ~/.config/nvim
+mkdir -p ~/.config
+cp -vr dotfiles/config/* ~/.config/
