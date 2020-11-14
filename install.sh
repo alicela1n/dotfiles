68972b482e35913a72df77a8d56faf6f52edffd8
Lyra <devfusediboot@protonmail.com>
2020-11-14 01:13:17 -0800

    added macOS configure script

--- install.sh
+++ install.sh
@@ -11,3 +11,9 @@ scripts/enable-services.sh
 # Set default shell to zsh
 scripts/set-default-to-zsh.sh
 echo "Done!"
+
+# Configuring macOS
+echo "macOS detected, running configure script..."
+if [[ $(uname -s) == "Darwin" ]]; then
+    scripts/configure_macos_defaults.sh
+fi
---
+++ scripts/configure_macos_defaults.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash 
+echo "DS_Store go away!..."
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
+echo "Enabling battery percentage..."
+defaults write com.apple.menuextra.battery ShowPercent -string "YES"
+
+echo "Showing all extensions..."
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+echo "Setting pink accent color..."
+defaults write NSGlobalDomain AppleAccentColor -integer 6
+
+echo "Setting pink highlight color..."
+defaults write NSGlobalDomain AppleHighlightColor -string "1.000000 0.749020 0.823529 Pink"
+
+echo "Disabling auto-correct..."
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+
+echo "Disabling auto-capitalization..."
+defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
+
+echo "Disabling quote substitution..."
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+
+echo "Disabling dash substitution..."
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+
+echo "Put drives on desktop..."
+defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
+
+echo "Making finder better..."
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+defaults write com.apple.finder ShowRecentTags -bool false
+defaults write com.apple.finder SidebarDevicesSectionDisclosedState -bool true
+defaults write com.apple.finder SidebarMediaBrowserSectionDisclosedState -bool true
+defaults write com.apple.finder SidebarPlacesSectionDisclosedState -bool true
+defaults write com.apple.finder SidebarSharedSectionDisclosedState -bool true
+defaults write com.apple.finder SidebarDevicesSectionDisclosedState -bool true
+defaults write com.apple.finder SidebarPlacesSectionDisclosedState -bool true
+defaults write com.apple.finder SidebarSharedSectionDisclosedState -bool true
+
+echo "Making the dock smaller..."
+defaults write com.apple.dock tilesize -int 30
