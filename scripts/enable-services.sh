72770776ba489cf1f06c3097bad100c8c0adc969
Alice <devfusediboot@protonmail.com>
2021-05-29 16:50:10 -0700

    fixed enable-services script that's been broken

--- scripts/enable-services.sh
+++ scripts/enable-services.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
-if [[ $(uname -s) == "Linux" ]];
-    if [[ $(file /sbin/init) == "/sbin/init: symbolic link to /lib/systemd/systemd" ]];
+if [[ $(uname -s) == "Linux" ]]; then
+    if [[ $(file /sbin/init) == "/sbin/init: symbolic link to /lib/systemd/systemd" ]]; then
         echo "Systemd detected"
         echo "Enabling services..."
         sudo systemctl enable --now libvirtd
     else
-        "unknown init system"
-    elif [[ $(uname -s) == "Darwin" ]]; then
+        echo "unknown init system"
+    fi
+elif [[ $(uname -s) == "Darwin" ]]; then
     echo "Nothing to enable atm"
 fi
