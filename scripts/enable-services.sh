d59db3758bdb5fe7951527e0814699ed0d6ffdb0
Alice <devfusediboot@protonmail.com>
2021-06-30 11:53:54 -0700

    fixed enable services

--- scripts/enable-services.sh
+++ scripts/enable-services.sh
@@ -1,11 +1,21 @@
 #!/usr/bin/env bash
-if [[ $(uname -s) == "Linux" ]];
-    if [[ $(file /sbin/init) == "/sbin/init: symbolic link to /lib/systemd/systemd" ]];
-        echo "Systemd detected"
-        echo "Enabling services..."
+if [[ "$(uname -s)" == "Linux" ]]; then
+    if [[ "$(file /sbin/init)" == "/sbin/init: symbolic link to ../lib/systemd/systemd" ]]; then
+        printf "Systemd detected\n"
+        printf "Enabling services...\n"
         sudo systemctl enable --now libvirtd
+    elif [[ "$(file /sbin/init)" == "/sbin/init: symbolic link to /lib/systemd/systemd" ]]; then
+        printf "Systemd detected\n"
+        printf "Enabling services...\n"
+        sudo systemctl enable --now libvirtd
+    elif [[ -f /usr/bin/openrc-init ]]; then
+        printf "OpenRC detected"
+        printf "Enabling services..."
+        sudo rc-update add libvirtd default
+        sudo /etc/init.d/libvirtd start
     else
-        "unknown init system"
-    elif [[ $(uname -s) == "Darwin" ]]; then
-    echo "Nothing to enable atm"
+        printf "unknown init system\n"
+    fi
+elif [[ "$(uname -s)" == "Darwin" ]]; then
+    printf "Nothing to enable atm\n"
 fi
