17196249ac1e408e00a0bb1b081b43efefb91b60
Alice S <quarkyalice@disroot.org>
2022-04-23 18:37:10 -0700

    update

--- scripts/configure_macos_defaults.sh
+++ scripts/configure_macos_defaults.sh
@@ -1,6 +1,7 @@
-#!/usr/bin/env bash 
+#!/usr/bin/env zsh
 echo "DS_Store go away!..."
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
 
 echo "Enabling battery percentage..."
 defaults write com.apple.menuextra.battery ShowPercent -string "YES"
@@ -45,3 +46,39 @@ defaults write com.apple.finder SidebarSharedSectionDisclosedState -bool true
 echo "Making the dock smaller..."
 defaults write com.apple.dock tilesize -int 40
 killall Dock
+
+echo "Stopping time machine from offering new disks for backup..."
+defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
+
+echo "Disabling annoying pop up alerts..."
+defaults write com.apple.LaunchServices LSQuarantine -bool false
+
+echo "Disabling verification on disk images..."
+defaults write com.apple.frameworks.diskimages skip-verify -bool true
+
+echo "Do not reopen previous content window on next launch please..."
+defaults write com.apple.systempreferences NSQuitAlwaysKeepsWindows -bool false
+defaults write com.apple.QuickTimePlayer NSQuitAlwaysKeepsWindows -int 0
+defaults write com.apple.Preview NSQuitAlwaysKeepsWindows -int 0
+
+echo "Disabling automatic termination of inactive apps..."
+defaults write NSGlobalDomain NSDisableAutomaticTermination -bool true
+defaults write com.apple.iChat NSDisableAutomaticTermination -bool yes
+
+echo "Enabling airdrop even on unsupported Macs and ethernet..."
+defaults write com.apple.NetworkBrowser BrowseAllInterfaces -bool true
+
+echo "Always showing scroll bars..."
+defaults write -g AppleShowScrollBars -string "Always"
+
+echo "Setting status bar clock..."
+defaults write com.apple.menuextra.clock "DateFormat" -string "\"EEE d MMM h:mm:ss\""
+
+echo "Showing library folder..."
+chflags nohidden ~/Library/
+
+echo "Disabling gatekeeper..."
+sudo spctl --master-disable
+
+echo "Disabling built in captive portal..."
+sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.captive.control Active -bool false
