f950dc0c81d2a9d2e996c6653b15b7c47bf40798
Alice S <quarkyalice@disroot.org>
2023-01-09 12:11:40 -0800

    update

--- scripts/install-etc-configs.sh
+++ scripts/install-etc-configs.sh
@@ -2,11 +2,11 @@
 if [[ -f /usr/bin/pacman ]]; then
     echo "Installing pacman configuration..."
     sudo cp -v /etc/pacman.conf /etc/pacman.conf.BACKUP
-    sudo cp -v etc/pacman.conf /etc/pacman.conf
+    sudo cp -vi etc/pacman.conf /etc/pacman.conf
 
     echo "Installing paru configuration..."
     sudo cp -v /etc/paru.conf /etc/paru.conf.BACKUP
-    sudo cp -v etc/paru.conf /etc/paru.conf
+    sudo cp -vi etc/paru.conf /etc/paru.conf
 fi
 sudo mkdir -pv /etc/profile.d
-sudo cp -v etc/profile.d/* /etc/profile.d
+sudo cp -vi etc/profile.d/* /etc/profile.d
