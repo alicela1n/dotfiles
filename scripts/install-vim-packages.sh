f2275b50dd2c5aadbd2e8859c6f22dba7dc32c6c
Alice <alice@alicela1n.space>
2025-07-20 11:56:00 -0700

    scripts/install-vim-packages.sh: Update

--- scripts/install-vim-packages.sh
+++ scripts/install-vim-packages.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 git clone https://github.com/vim-airline/vim-airline ~/.vim/pack/dist/start/vim-airline
 git clone https://github.com/rust-lang/rust.vim ~/.vim/pack/plugins/start/rust.vim
-
+git clone https://github.com/kana/vim-operator-user.git ~/.vim/pack/plugins/start/vim-operator-user
+git clone https://github.com/Shougo/vimproc.vim.git ~/.vim/pack/plugins/start/vimproc.vim
+git clone https://github.com/rhysd/vim-clang-format.git ~/.vim/pack/plugins/start/vim-format-clang
