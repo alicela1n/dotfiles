87ac6fd31c45da9e6a0f8a818869eedaedc3a329
Alice <alice@alicela1n.space>
2025-07-20 12:24:29 -0700

    Update macOS packages

--- scripts/install-packages-macos.sh
+++ scripts/install-packages-macos.sh
@@ -3,12 +3,7 @@
 echo "Installing homebrew..."
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 echo "Installing homebrew casks..."
-cat packages/homebrew-cask | xargs brew install --cask
+cat packages/homebrew-cask | xargs brew install --cask --force
 
-if [ -f /opt/local/bin/port ]; then
-    echo "Installing MacPorts packages..."
-    cat packages/macports | xargs sudo port install
-else
-    echo "Installing homebrew packages..."
-    cat packages/homebrew | xargs brew install
-fi
+echo "Installing homebrew packages..."
+cat packages/homebrew | xargs brew install
