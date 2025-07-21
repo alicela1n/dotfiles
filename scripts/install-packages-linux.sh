bfd495e6aa1d73c17156cdc835dc09b29979317b
Alice <alice@alicela1n.space>
2025-07-21 16:02:33 -0700

    Add fedora packages and other updates

--- scripts/install-packages-linux.sh
+++ scripts/install-packages-linux.sh
@@ -1,9 +1,40 @@
 #!/usr/bin/env bash
-echo "Installing linuxbrew..."
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if [[ -f /usr/bin/flatpak ]]; then
+    echo "Removing fedora flatpak repo..."
+    flatpak remote-delete fedora
 
-echo "Installing homebrew packages..."
-cat packages/homebrew | xargs brew install
+    echo "Adding flathub..."
+    flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+    flatpak install --assumeyes $(cat packages/flatpak)
+fi
+
+if [[ -f /usr/bin/dnf ]]; then
+    echo "Enabling rpmfusion..."
+    sudo dnf install \
+  https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+    sudo dnf install \
+  https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+    sudo dnf group upgrade -y core
+
+    echo "Updating all packages..."
+    sudo dnf update -y
+
+    echo "Installing development tools..."
+    sudo dnf group install -y development-tools
+    
+    echo "Instaling the rest of the packages..."
+    sudo dnf install -y $(cat packages/fedora)
+else
+    echo "Installing linuxbrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+    echo "Installing homebrew packages..."
+    cat packages/homebrew | xargs brew install
+fi
+
+echo "Installing tailscale..."
+curl -fsSL https://tailscale.com/install.sh | sh
 
 echo "Installing Jetbrains Toolbox..."
 scripts/install-jetbrains-toolbox-linux.sh
+
--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 echo "Installing packages..."
 
-if [[ -f /usr/bin/flatpak ]]; then
-    echo "Executing flatpak script..."
-    scripts/install-packages-flatpak.sh
-fi
-
 if [[ $(uname -s) == "Darwin" ]]; then
     echo "Executing macOS script..."
     scripts/install-packages-macos.sh
