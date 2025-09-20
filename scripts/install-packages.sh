48e4f6ef28f995a5c8123d8cdd9847c1bca879d4
Alice <alice@alicela1n.space>
2025-09-20 15:34:54 -0700

    Add confirmation for install packages script

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 echo "Installing packages..."
 
-if [[ $(uname -s) == "Darwin" ]]; then
-    echo "Executing macOS script..."
-    scripts/install-packages-macos.sh
-elif [[ $(uname -s) == "Linux" ]]; then
-    echo "Executing Linux script..."
-    scripts/install-packages-linux.sh
+read -p "Do you want to install packages? [y/N]" response
+
+if [[ "$response" =~ ^(yes|y|Yes|Y)$ ]]; then
+    if [[ $(uname -s) == "Darwin" ]]; then
+        echo "Executing macOS script..."
+        scripts/install-packages-macos.sh
+    elif [[ $(uname -s) == "Linux" ]]; then
+        echo "Executing Linux script..."
+        scripts/install-packages-linux.sh
+    fi
 fi
