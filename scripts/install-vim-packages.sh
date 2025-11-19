4e51de4a972db0157ce8766f909164bf31b2ac30
Alice <alice@alicela1n.space>
2025-11-19 15:38:33 -0800

    Add vim-dispatch and reorganize packages

--- scripts/install-vim-packages.sh
+++ scripts/install-vim-packages.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
-git clone https://github.com/vim-airline/vim-airline ~/.vim/pack/dist/start/vim-airline
+git clone https://github.com/vim-airline/vim-airline ~/.vim/pack/plugins/start/vim-airline
 git clone https://github.com/rust-lang/rust.vim ~/.vim/pack/plugins/start/rust.vim
 git clone https://github.com/kana/vim-operator-user.git ~/.vim/pack/plugins/start/vim-operator-user
 git clone https://github.com/Shougo/vimproc.vim.git ~/.vim/pack/plugins/start/vimproc.vim
 git clone https://github.com/rhysd/vim-clang-format.git ~/.vim/pack/plugins/start/vim-clang-format
 git clone https://github.com/vimwiki/vimwiki.git ~/.vim/pack/plugins/start/vimwiki
-git clone https://github.com/preservim/nerdtree.git ~/.vim/pack/vendor/start/nerdtree
-git clone https://github.com/puremourning/vimspector.git ~/.vim/pack/vendor/start/vimspector
-git clone https://github.com/dbeniamine/vim-compile.git ~/.vim/pack/vendor/start/vim-compile
+git clone https://github.com/preservim/nerdtree.git ~/.vim/pack/plugins/start/nerdtree
+git clone https://github.com/puremourning/vimspector.git ~/.vim/pack/plugins/start/vimspector
+git clone https://github.com/dbeniamine/vim-compile.git ~/.vim/pack/plugins/start/vim-compile
+git clone https://github.com/tpope/vim-dispatch.git ~/.vim/pack/plugins/start/vim-dispatch
