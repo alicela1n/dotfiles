0c0f6e9a6400d0e48c53e422e3980f5db27c73a7
Alice <alice@alicela1n.space>
2026-05-28 18:33:10 -0700

    Update neovim configs

---
+++ dotfiles/config/nvim/lua/plugins/flouromachine.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "maxmx03/fluoromachine.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      local fm = require("fluoromachine")
+
+      fm.setup({
+        glow = true,
+        theme = "fluoromachine",
+        transparent = true,
+      })
+
+      vim.cmd.colorscheme("fluoromachine")
+    end,
+  },
+}
