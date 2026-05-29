18c6abd8ea3fb31332bd76b0edcefb204b811dde
Alice <alice@alicela1n.space>
2026-05-28 20:26:12 -0700

    Disable noice cmdline bar

---
+++ dotfiles/config/nvim/lua/plugins/noice.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "folke/noice.nvim",
+    opts = {
+      cmdline = {
+        view = "cmdline",  -- puts it back at the bottom like normal vim
+      },
+      messages = {
+        enabled = false,   -- optional: disable message routing through noice
+      },
+    },
+  },
+}
