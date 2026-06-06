251ebc35760f281875dea9b68e5ff89a9697fac0
Alice <alice@alicela1n.space>
2026-06-05 18:33:23 -0700

    Update nvim configs

--- dotfiles/config/nvim/init.lua
+++ dotfiles/config/nvim/init.lua
@@ -1,2 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Leaving this empty will use the default options from the
+-- `nvim-lspconfig` plugin. See |:h vim.lsp.Config| for all
+-- available fields, and see below for more customization.
