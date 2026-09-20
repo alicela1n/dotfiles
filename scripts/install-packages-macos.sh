b2eb75033a9a66edc92c81883020c5518cbbab46
Alice <alice@alicela1n.space>
2026-09-20 16:04:42 -0700

    More changes

--- scripts/install-packages-macos.sh
+++ scripts/install-packages-macos.sh
@@ -2,6 +2,13 @@
 
 echo "Installing homebrew..."
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+if ! type "brew" > /dev/null; then
+	echo >> /Users/alice/.zprofile
+	echo 'eval "$(/opt/homebrew/bin/brew shellenv zsh)"' >> /Users/alice/.zprofile
+	eval "$(/opt/homebrew/bin/brew shellenv zsh)"
+fi
+
 echo "Installing homebrew casks..."
 cat packages/homebrew-cask | xargs brew install --cask --force
 
